39e9c8bfd8baa77c79d33eb043bb308eaa380475
Sandro Pasquali <spasquali@appnexus.com>
2013-08-16 15:10:33 -0400

    cosmetics

--- ellipster.js
+++ ellipster.js
@@ -1,16 +1,15 @@
 (function($) {
     $.fn.ellipster = function(options) {
         var defaults = {
-            'char' : '...',
-            'background' : 'white',
-            'link'	: null
+            'char' 			: '...',
+            'background' 	: 'white',
+            'link'			: null
         };
 
         options = $.extend(defaults, options);
         this.each(function() {
         
             var $this	= $(this);
-            var text 	= $this.text();
             var lineHeight =  $this.css("line-height").replace('px','');
             var bs = '-2px 0px 4px ' + options.background; 
             
@@ -19,12 +18,12 @@
             var $ellipster = $(!options.link ? '<div></div>' : '<a href="' + options.link + '"></a>');
             $ellipster
             .css({
-            	position: 'absolute',
-        		right: 0,
-        		top: $this.height() - lineHeight,
-        		'padding-left' : '2px',
-        		height: lineHeight,
-        		backgroundColor: options.background,
+            	position			: 'absolute',
+        		right				: 0,
+        		top					: $this.height() - lineHeight,
+        		'padding-left' 		: '2px',
+        		height				: lineHeight,
+        		backgroundColor		: options.background,
         		'-moz-box-shadow'	: bs,
 				'-webkit-box-shadow': bs,
 				'box-shadow'		: bs
